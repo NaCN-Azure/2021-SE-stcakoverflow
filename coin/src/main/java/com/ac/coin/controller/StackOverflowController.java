a3d83633577d6170ae4a712776ad70188b33270e
郑镕圭 <1271556357@qq.com>
2021-12-15 00:44:25 +0800

    fix: 修改controller的get方法

--- coin/src/main/java/com/ac/coin/controller/StackOverflowController.java
+++ coin/src/main/java/com/ac/coin/controller/StackOverflowController.java
@@ -3,6 +3,7 @@ package com.ac.coin.controller;
 import com.ac.coin.service.StackOverflowService;
 import com.ac.coin.vo.ResponseVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
@@ -35,31 +36,37 @@ public class StackOverflowController {
     }
 
     @GetMapping("/findTargetNodesInfo")
+    @ResponseStatus(value= HttpStatus.OK)
     public ResponseVO findTargetNodesInfo(@RequestParam("name") String name){
         return ResponseVO.buildSuccess(stackOverflowService.findTargetSubTag(name));//本方法返回单独tag(给图谱展示用的)及其子节点
     }
 
     @GetMapping("/findTargetNodesDescription")
+    @ResponseStatus(value= HttpStatus.OK)
     public ResponseVO findTargetNodesDescription(@RequestParam("name") String name){
         return ResponseVO.buildSuccess(stackOverflowService.findTargetTag(name));//本方法返回单独tag介绍（返回一个完整的tag类型，前端可能需要新定义方法）
     }
 
     @GetMapping("/findTargetNodesRelated")
+    @ResponseStatus(value= HttpStatus.OK)
     public ResponseVO findTargetNodesRelated(@RequestParam("name") String name){
         return ResponseVO.buildSuccess(stackOverflowService.findTargetSubRelation(name));//本方法返回单独tag及其附属关系(给图谱展示用的)
     }
 
     @GetMapping("/findTargetQuestions")
+    @ResponseStatus(value= HttpStatus.OK)
     public ResponseVO findTargetQuestions(@RequestParam("name") String name) {
         return ResponseVO.buildSuccess(stackOverflowService.findHotQuestions(name));//返回最热五个问题
     }
 
     @GetMapping("/findTargetNodesChart")
+    @ResponseStatus(value= HttpStatus.OK)
     public ResponseVO findTargetNodesChart(@RequestParam("name") String name){
         return ResponseVO.buildSuccess(stackOverflowService.findTargetNodesChart(name)); //输入一个tagname，返回他的折线图信息
     }
 
     @GetMapping("/findNodesFuzzy")
+    @ResponseStatus(value= HttpStatus.OK)
     public ResponseVO findNodesFuzzy(@RequestParam("name") String name){
         return ResponseVO.buildSuccess(stackOverflowService.findNodesFuzzy(name));
     }
